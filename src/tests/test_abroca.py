bb5494388117b1d269da2b8b29155a59d8a3239b
nathalierze <nathalie.rzepka@htw-berlin.de>
2023-03-11 15:54:17 +0100

    added test files

---
+++ src/tests/test_abroca.py
@@ -0,0 +1,9 @@
+import unittest
+from abroca import calculate_abroca
+
+class TestCalculateAbroca(unittest.TestCase):
+    
+    def test_calculate_abroca(self):
+        self.AssertEqual(1,1)
+
+unittest.main()
