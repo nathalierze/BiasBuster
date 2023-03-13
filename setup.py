7dc2ae64649ec3cbb29e730820663d59a2c1f739
nathalierze <nathalie.rzepka@htw-berlin.de>
2023-03-13 19:26:46 +0100

    changed version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding = "utf-8") as fh:
 
 setuptools.setup(
     name = "BiasBuster",
-    version = "0.0.2",
+    version = "0.0.3",
     author = "Nathalie Rzepka",
     # author_email = "author@example.com",
     description = "A Python package to check for algorithmic bias.",
