f3ae0b37fa5965d3c00bacbb9d2d4774d97937d3
nathalierze <nathalie.rzepka@htw-berlin.de>
2023-03-13 19:04:23 +0100

    changed name

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ with open("README.md", "r", encoding = "utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name = "bias-metrics",
+    name = "BiasBuster",
     version = "0.0.2",
     author = "Nathalie Rzepka",
     # author_email = "author@example.com",
     description = "A Python package to check for algorithmic bias.",
     long_description = long_description,
     long_description_content_type = "text/markdown",
-    url = "https://github.com/nathalierze/bias-metrics",
+    url = "https://github.com/nathalierze/BiasBuster",
     project_urls = {
-        "Bug Tracker": "https://github.com/nathalierze/bias-metrics/issuesL",
+        "Bug Tracker": "https://github.com/nathalierze/BiasBuster/issuesL",
     },
     classifiers = [
         "Programming Language :: Python :: 3",
